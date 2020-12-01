c7b3bfb6af55df3376614cfdda574eab9dbe436c
wangzhen12321 <2462667514@qq.com>
2020-12-01 19:12:55 +0800

    第7次提交
    Signed-off-by: wangzhen12321 <2462667514@qq.com>

--- src/com/wz/state/TestPC.java
+++ src/com/wz/state/TestPC.java
@@ -19,7 +19,7 @@ class Productor extends Thread{
         this.container=synContainer;
     }
 
-    //生产sds442525
+    //生产
     @Override
     public void run() {
         for (int i = 0; i < 100; i++) {
